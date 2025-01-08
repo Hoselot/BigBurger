1eb32122735f2f9bbf6b7c9465855fbc4df56545
Joaquin Cangialosi <joaquincangialosidp@gmail.com>
2025-01-08 13:10:25 -0300

    feat:"Creación de archivos básicos back"

---
+++ Backend/src/main/java/com/bigburger/bigburger/BigburgerApplication.java
@@ -0,0 +1,13 @@
+package com.bigburger.bigburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigburgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigburgerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/bigburger/bigburger/BigburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigburger.bigburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigburgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
