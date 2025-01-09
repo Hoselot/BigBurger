892c022e86032000e4fe5eb921dab4f972c72bb1
EmiArias021 <64331865+EmilianoArias021@users.noreply.github.com>
2025-01-09 11:01:16 -0300

    chore: crear estructura inicial del proyecto

---
+++ Frontend/src/Pages/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainPage.css';
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      <h1>Bienvenido a la Página Principal</h1>
+      <p>Esta es la página principal de la aplicación.</p>
+    </div>
+  );
+}
+
+export default MainPage;
