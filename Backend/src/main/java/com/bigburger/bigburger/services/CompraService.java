e2900543a0a8a18b54a3f7749c9cb08d836547de
EmiArias021 <emilianoariasjesus86@gmail.com>
2025-05-20 15:28:24 -0300

    cambio

--- Backend/src/main/java/com/bigburger/bigburger/services/CompraService.java
+++ Backend/src/main/java/com/bigburger/bigburger/services/CompraService.java
@@ -2,6 +2,7 @@ package com.bigburger.bigburger.services;
 
 import com.bigburger.bigburger.models.DetallePedidoModel;
 import com.bigburger.bigburger.models.PedidoModel;
+import com.bigburger.bigburger.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +16,13 @@ public class CompraService {
     private MercadoPagoService mercadoPagoService;
     @Autowired
     private PedidoService pedidoService;
+    @Autowired
+    private PedidoRepository pedidoRepository;
 
     public String RealizarCompra(String nombre, String apellido, String telefono, String gmail,
                                  BigDecimal delivery, List<DetallePedidoModel> detallePedido) throws Exception{
         PedidoModel pedidoModel = pedidoService.crearPedido(nombre,apellido,telefono,gmail,delivery,detallePedido);
+        pedidoRepository.save(pedidoModel);
         return mercadoPagoService.crearPreferenciaDePago(nombre,apellido,telefono,gmail,pedidoModel);
     }
 
